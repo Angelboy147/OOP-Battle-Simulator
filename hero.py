8e958245ac0c1c07dc0949d8d5db9d9f4214c5be
Angelboy147 <daniel.mullens@ascte.org>
2025-09-03 10:08:14 -0500

    Setting up hero

--- hero.py
+++ hero.py
@@ -14,8 +14,11 @@ class Hero:
     
     def __init__(self, name):
         #TODO Set the hero's name.
+        self.name = name
         #TODO Set the hero's health. You might give the hero more health than a goblin.
+        self.health = 175 
         #TODO Set the hero's attack power. Should it be more consistent than the goblin's?
+        self.attack_power = random.randint(5, 15)
     
 
     def strike(self):
